7a900cfe89f923450893df0346383fa1a7531267
Himanshu8Mittal <himanshum21@iitk.ac.in>
2024-07-31 14:56:48 +0530

    Changed the Dataabse URL

--- src/db/model/database.py
+++ src/db/model/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://mywebuser:Password@localhost:5432/company"
+# SQLALCHEMY_DATABASE_URL = "postgresql://mywebuser:Password@localhost:5432/company"
+SQLALCHEMY_DATABASE_URL = "postgresql://mywebuser:HsSU5oVz57wYdzB9yw8j3nraoYpv3jh2@dpg-cql04rt6l47c73f0ha90-a.oregon-postgres.render.com/company_pvjs"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
